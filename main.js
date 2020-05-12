e702db8fa97f63faba03b5cb87932a1d5df9b768
hujinqiang <huzewei@dingtalk.com>
2020-05-12 16:07:44 +0800

    阿拉丁集成

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import App from './App'
 import MinRequest from './utils/request.js'
 import minRequest from './api/index.js'
 import COMMON from './utils/common.js'
+import './static/sdk/ald-stat.js'
 
 Vue.use(MinRequest)
 Vue.config.productionTip = false
