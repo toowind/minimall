812c10c900f300bd3171a50fa4611c26136dc3bb
贾轲-H5 <jk@jk.com>
2020-05-13 10:05:51 +0800

    hs

--- utils/env.js
+++ utils/env.js
@@ -9,11 +9,11 @@ let BASE_API = '';
 
 if (process.env.NODE_ENV == 'development') {
     // 编译环境
-    // BASE_API = 'https://shop.tgfwe.com';
-    BASE_API = 'https://shop-t.baertt.com';
+    BASE_API = 'https://shop-t.baertt.com'; // 测试
   }else if(process.env.NODE_ENV == 'production'){
-    // 线上环境
-    BASE_API = 'https://kd-api-shop.baertt.com';
+	// 线上环境
+	// BASE_API = 'https://kd-api-shop.baertt.com'; // 线上
+    BASE_API = 'https://shop-t.baertt.com/'; // 测试 体验版使用
 }
 
 export {
